79b32927a7f116724db01568b3dea823ba639564
Nan Jiang <njiang028@gmail.com>
2020-10-15 17:11:42 -0400

    Add a web hook to sync content to kinto

--- contentful-proxy/index.js
+++ contentful-proxy/index.js
@@ -1,12 +1,25 @@
 const contentful = require("contentful");
+const kinto = require("kinto-http");
 const { transform } = require("./transform");
 
+// Contentful client
 const client = contentful.createClient({
   space: process.env.SPACE_ID,
   accessToken: process.env.ACCESS_TOKEN,
 });
 
-async function fetchContent() {
+const auth = Buffer.from(
+  `${process.env.KINTO_USER}:${process.env.KINTO_PASSWORD}`
+).toString("base64");
+// Kinto client
+const rsClient = new kinto.default("https://kinto.dev.mozaws.net/v1", {
+  headers: {
+    Authorization: `Basic ${auth}`,
+  },
+});
+const kintoStore = rsClient.bucket("main").collection("whats-new-panel");
+
+async function fetchAll() {
   const entries = await client.getEntries({
     content_type: "whatsNewPanel",
     include: 3,
@@ -14,8 +27,19 @@ async function fetchContent() {
   return entries.items.map((entry) => entry.fields);
 }
 
+async function fetchOne(id) {
+  const entry = await client.getEntry(id, {
+    content_type: "whatsNewPanel",
+    include: 3,
+  });
+  return entry.fields;
+}
+
 /**
- * HTTP Cloud Function to fetch Whats-New-Panel messages from Contentful content API.
+ * HTTP Cloud Function to:
+ *
+ *  * [GET] fetch Whats-New-Panel messages from Contentful content API
+ *  * [POST] notify a new content is published on Contentful
  *
  * @param {Object} req Cloud Function request context.
  *                     More info: https://expressjs.com/en/api.html#req
@@ -23,11 +47,54 @@ async function fetchContent() {
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.wnpGET = async (req, res) => {
-  try {
-    const entries = await fetchContent();
-    const messages = transform(entries);
-    res.json({ messages });
-  } catch (err) {
-    res.error(500).json({ error: `${err}` });
+  res.set("Access-Control-Allow-Origin", "*");
+  switch (req.method) {
+    case "OPTIONS":
+      res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      res.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Content-Length, X-Contentful-Webhooks-Token, x-contentful-topic"
+      );
+      res.set("Access-Control-Max-Age", "3600");
+      res.status(204).send("");
+      break;
+    case "GET":
+      try {
+        const entries = await fetchAll();
+        const messages = transform(entries);
+        res.json({ messages });
+      } catch (err) {
+        res.status(500).json({ error: `${err}` });
+      }
+      break;
+    case "POST":
+      if (
+        !req.get("X-Contentful-Webhooks-Token") ||
+        req.get("X-Contentful-Webhooks-Token") !== process.env.WEB_HOOKS_TOKEN
+      ) {
+        res.status(401).json({ error: "Unauthorized request" });
+      }
+      try {
+        const entry = await fetchOne(req.body.sys.id);
+        const [parsed] = transform([entry]);
+        const [_type, _entry, publishState] = req
+          .get("x-contentful-topic")
+          .split(".");
+        if (publishState === "publish") {
+          kintoStore.createRecord(parsed);
+          res.status(201).json({ message: "ok" });
+        } else if (publishState === "unpublish") {
+          kintoStore.deleteRecord(parsed.id);
+          res.status(200).json({ message: "ok" });
+        } else {
+          res.status(404).json({ error: "Invalid action" });
+        }
+      } catch (err) {
+        res.status(500).json({ error: `${err}` });
+      }
+      break;
+    default:
+      res.status(405).send({ error: "Unsupported HTTP method" });
+      break;
   }
 };
